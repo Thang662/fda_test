3e459d5ceb3bf9ee040ccb85f9783bf5e4bcff06
Thắng Lê Trần <letranthang0606@gmail.com>
2023-04-07 01:25:30 +0700

    change codes

--- model/deeplab.py
+++ model/deeplab.py
@@ -177,6 +177,8 @@ class ResNet101(nn.Module):
             self.loss_ent = ent.mean()
 
             x = nn.functional.interpolate(x, size=(h, w), mode='bilinear', align_corners=True)
+            print(x.size())
+            print(lbl.size())
             if lbl is not None:
                 self.loss_seg = self.CrossEntropy2d(x, lbl, weight=weight)
 
